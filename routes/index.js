6504b80965932bbc668e521bf901b9015338093a
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2023-07-26 17:25:10 +0700

    menambahkan tulisan baru

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.send("HELLO LUTFI");
+  //res.send("HELLO LUTFI");
+  res.send(`WEBSITE INI DALAM PROSES PEMBANGUNAN`);
 });
 
 module.exports = router;
